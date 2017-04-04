97d43524157307fc6d6cec3a5080e9e02001dbd5
Jordan Hudgens <dakotajennings@icloud.com>
2017-04-04 03:22:08 +0000

    Jennings tool initial commit

---
+++ lib/jennings_view_tool.rb
@@ -0,0 +1,6 @@
+require "jennings_view_tool/version"
+require "jennings_view_tool/renderer"
+
+module JenningsViewTool
+  # Your code goes here...
+end
---
+++ lib/jennings_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JenningsViewTool
+  VERSION = "0.1.0"
+end
